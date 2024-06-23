954d11dbb93878da1070b4eb94109347b5b7f6bd
Dudols <deveduardotxalves@gmail.com>
2024-06-22 21:25:11 -0300

    🚀 feat: remove unused features

--- Airbnb_Rio_PricePrediction_Deploy.py
+++ Airbnb_Rio_PricePrediction_Deploy.py
@@ -3,8 +3,8 @@ import streamlit as st
 import joblib
 
 
-x_numeric = {'host_total_listings_count': 0,'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0, 
-             'extra_people': 0, 'minimum_nights': 0, 'number_of_reviews': 0, 'year': 0, 'month': 0, 'n_amenities': 0}
+x_numeric = {'latitude': 0, 'longitude': 0, 'accommodates': 0, 'bathrooms': 0, 'bedrooms': 0, 'beds': 0, 
+             'extra_people': 0, 'year': 0, 'month': 0}
 
 x_tf = {'host_is_superhost': 0, 'host_identity_verified': 0, 'instant_bookable': 0}
 
